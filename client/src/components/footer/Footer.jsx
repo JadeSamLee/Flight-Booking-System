ad137b4388a2648844369798d242d09e04d2fcdc
JadeSamLee <113660177+JadeSamLee@users.noreply.github.com>
2024-12-04 03:41:01 +0530

    update of functionalities

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+  import React from 'react';
+  import './footer.css';
+
+  const Footer = () => {
+    return (
+      <div className='footer_main'>
+        <div className='footer_footer'>
+          <p>JetSetGo</p>
+          <p>&copy; All Rights Reserved 2024</p>
+        </div>
+      </div>
+    );
+  };
+
+  export default Footer;
---
+++ client/src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.css';
+import Footer from '../footer/Footer';
+
+const Main = () => {
+  return (
+    <div className='main_main'>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
